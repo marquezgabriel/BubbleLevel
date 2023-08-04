55685ea20a9f8e9055b394b8065698e209647ef9
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-08-03 20:17:06 -0400

    Completed

---
+++ Bubble Level/BubbleExtension.swift
@@ -0,0 +1,21 @@
+//
+//  BubbleExtension.swift
+//  Bubble Level
+//
+//  Created by Gabriel Marquez on 2023-08-03.
+//
+
+import Foundation
+
+extension Double {
+    func describeAsFixedLengthString(integerDigits: Int = 2, fractionDigits: Int = 2) -> String {
+        self.formatted(
+            .number
+                .sign(strategy: .always())
+                .precision(
+                    .integerAndFractionLength(integer: integerDigits, fraction: fractionDigits)
+                )
+        )
+    }
+}
+
