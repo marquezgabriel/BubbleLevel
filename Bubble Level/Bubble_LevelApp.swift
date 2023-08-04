9298683ddfe7b0af2fa800e316c4532cb67612b3
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-08-03 20:06:12 -0400

    Initial Commit

---
+++ Bubble Level/Bubble_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_LevelApp.swift
+//  Bubble Level
+//
+//  Created by Gabriel Marquez on 2023-08-03.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_LevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
